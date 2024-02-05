fa7587c6e5322258befc2d62e638a4f54c45464b
Jose Cuenca <jcuenca@unsa.edu.pe>
2024-02-05 16:50:36 -0500

    Lluvia de Meteoritos

--- SpaceGame/Assets/Scripts/CheckPoint.cs
+++ SpaceGame/Assets/Scripts/CheckPoint.cs
@@ -7,17 +7,30 @@ public class CheckPoint : MonoBehaviour
     [SerializeField] GameObject player;
     [SerializeField] List<GameObject> checkpoints;
     private Vector3 lastCheckpointPosition;
+    private Quaternion lastCheckpointRotation;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("KillZone") && lastCheckpointPosition != null)
         {
             player.transform.position = lastCheckpointPosition;
+            player.transform.rotation = lastCheckpointRotation;
         }
         else if (checkpoints.Contains(other.gameObject))
         {
             lastCheckpointPosition = other.transform.position;
-            Destroy(other.gameObject);  
+            lastCheckpointRotation = other.transform.rotation;
+            Destroy(other.gameObject);
         }
     }
+
+    public void ResetToLastCheckpoint()
+    {
+        if (lastCheckpointPosition != null)
+        {
+            player.transform.position = lastCheckpointPosition;
+            player.transform.rotation = lastCheckpointRotation;
+        }
+    }
+
 }
