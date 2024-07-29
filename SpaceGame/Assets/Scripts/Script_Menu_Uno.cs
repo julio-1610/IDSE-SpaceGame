e73e8a1d94c86008439319bd86dc6b6ef9a99c3c
agarciapu <64229084+agarciapu@users.noreply.github.com>
2024-07-28 23:11:18 -0500

    scripts new

---
+++ SpaceGame/Assets/Scripts/Script_Menu_Uno.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Script_Menu_Uno : MonoBehaviour
+{
+    public void EmpezarNivel(string NombreNivel)
+    {
+        SceneManager.LoadScene(NombreNivel);
+    }
+    
+    public void Salir()
+    {
+        Application.Quit();
+        Debug.Log("Se cierra el juego");
+    }
+}
